452f76ce456ada198204133347794fd78cb05e2d
Duvvu Lakshmi Prasanna <prasannayadav2468@gmail.com>
2026-05-01 22:07:55 +0530

    last update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import istanbul from 'vite-plugin-istanbul'
 
 export default defineConfig({
-  base: '/',
+  base: '/Analytics-dashboard-cypress-e2e-new/',
   build: {
     sourcemap: true,
   },
